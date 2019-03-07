0f274a3dd662e31be87c92c25e676a8196486993
Mikhayl Joshua Castro Abellanosa <32891934+luckeyepe@users.noreply.github.com>
2019-03-07 18:17:16 +0800

    activityMenuItemDetails created new

---
+++ app/src/main/java/mjabellanosa02/gmail/com/MenuItemDetails.kt
@@ -0,0 +1,12 @@
+package mjabellanosa02.gmail.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MenuItemDetails : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_menu_item_details)
+    }
+}
