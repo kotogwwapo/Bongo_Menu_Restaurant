037b53d2f87b713b4b0f2411b4c5c8e0c0ef5a8c
Mikhaelo Nheal Bongo <kaylokoto@gmail.com>
2019-03-08 16:11:03 +0800

    your cart activity created and its rows

---
+++ app/src/main/java/mjabellanosa02/gmail/com/YourCartActivity.kt
@@ -0,0 +1,12 @@
+package mjabellanosa02.gmail.com
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class YourCartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_your_cart)
+    }
+}
